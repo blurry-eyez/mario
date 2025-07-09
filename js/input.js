a30582f420712140ea29bc501ac60a501d0c0b33
Blurry Eyes <77361124+blurry-eyez@users.noreply.github.com>
2025-07-10 04:07:26 +0530

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,54 +1,56 @@
-(function() {
+(function () {
     var pressedKeys = {};
 
     function setKey(event, status) {
-        var code = event.keyCode;
+        var code = event.keyCode || event.which || 0;
         var key;
 
-        switch(code) {
-        case 32:
-            key = 'SPACE'; break;
-        case 37:
-            key = 'LEFT'; break;
-        case 38:
-            key = 'UP'; break;
-        case 39:
-            key = 'RIGHT'; break;
-        case 40:
-            key = 'DOWN'; break;
-        case 88:
-            key = 'JUMP'; break;
-        case 90:
-            key = 'RUN'; break;
-        default:
-            key = String.fromCharCode(code);
+        // Normalize from .code if needed (for synthetic events)
+        if (event.code === 'ArrowLeft') code = 37;
+        else if (event.code === 'ArrowRight') code = 39;
+        else if (event.code === 'KeyX') code = 88;
+        else if (event.code === 'KeyZ') code = 90;
+
+        switch (code) {
+            case 32:
+                key = 'SPACE'; break;
+            case 37:
+                key = 'LEFT'; break;
+            case 38:
+                key = 'UP'; break;
+            case 39:
+                key = 'RIGHT'; break;
+            case 40:
+                key = 'DOWN'; break;
+            case 88:
+                key = 'JUMP'; break;
+            case 90:
+                key = 'RUN'; break;
+            default:
+                key = String.fromCharCode(code);
         }
 
         pressedKeys[key] = status;
     }
 
-    document.addEventListener('keydown', function(e) {
+    document.addEventListener('keydown', function (e) {
         setKey(e, true);
     });
 
-    document.addEventListener('keyup', function(e) {
+    document.addEventListener('keyup', function (e) {
         setKey(e, false);
     });
 
-    window.addEventListener('blur', function() {
+    window.addEventListener('blur', function () {
         pressedKeys = {};
     });
 
     window.input = {
-        isDown: function(key) {
+        isDown: function (key) {
             return pressedKeys[key.toUpperCase()];
         },
-        reset: function() {
-          pressedKeys['RUN'] = false;
-          pressedKeys['LEFT'] = false;
-          pressedKeys['RIGHT'] = false;
-          pressedKeys['DOWN'] = false;
-          pressedKeys['JUMP'] = false;
+        reset: function () {
+            pressedKeys = {};
         }
     };
 })();
